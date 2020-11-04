7c52c6fa42dfdc8299b633790017f4075ddde50a
Frans Beets <scorchedearth777@gmail.com>
2020-11-04 00:29:14 -0600

    Página carrito de compras y botones

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = app => {
-
+    app.get('/carrito', (req, res) => {
+        res.render("carro");
+    });
     app.get('/', (req, res) => {
         res.render("home");
     });
-
     app.use((req, res, next) => {
         res.status(404).render("error404")
     });
